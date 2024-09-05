ccaeafeeb688fdbcd307ad259cd4d1651d0b24d3
Vinícius Falcão <andrefalcaov8@gmail.com>
2024-09-05 12:54:55 -0300

    Add project files.

---
+++ Test-Gera-Pdf/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using iText.Html2pdf;
+
+Console.WriteLine("Aplicação Iniciada");
+Console.WriteLine("Passe o caminho do Arquivo: ");
+
+string caminhoDoArquivoHTML = @"";
+string caminhoDoArquivoPDF = @""; 
+if (File.Exists(caminhoDoArquivoHTML))
+{
+    using (FileStream htmlSource = File.Open(caminhoDoArquivoHTML, FileMode.Open))
+    using (FileStream pdfDest = File.Open(caminhoDoArquivoPDF, FileMode.Create))
+    {
+        ConverterProperties converterProperties = new ConverterProperties();
+        HtmlConverter.ConvertToPdf(htmlSource, pdfDest, converterProperties);
+    }
+
+}
+   
+ Console.WriteLine("Arquivo não encontrado");
+
